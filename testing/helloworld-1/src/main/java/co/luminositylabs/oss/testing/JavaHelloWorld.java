f3b6a304f5f78e48e58cdc07a6b4137a8bf48070
Phillip Ross <phillip.w.g.ross@gmail.com>
2024-03-20 07:20:31 -0400

    pom.xml cleanup and hello world documentation fixes
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>

--- testing/helloworld-1/src/main/java/co/luminositylabs/oss/testing/JavaHelloWorld.java
+++ testing/helloworld-1/src/main/java/co/luminositylabs/oss/testing/JavaHelloWorld.java
@@ -3,6 +3,9 @@ package co.luminositylabs.oss.testing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A simple hello world implemented directly in java.
+ */
 public class JavaHelloWorld {
 
     /** The static logger instance. */
@@ -11,10 +14,19 @@ public class JavaHelloWorld {
     /** The prefix for the greeting. */
     public static final String GREETING_PREFIX = "Hello ";
 
+    /** Default no-args constructor. */
+    protected JavaHelloWorld() {
+    }
+
+    /**
+     * Print and return a greeting for a specified name.
+     *
+     * @param name the name for the greeting
+     * @return the greeting
+     */
     public String sayHello(final String name) {
         final String greeting = GREETING_PREFIX + name;
         logger.debug(greeting);
         return greeting;
     }
-
 }
