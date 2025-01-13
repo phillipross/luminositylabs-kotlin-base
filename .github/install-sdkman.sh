450f22d652cb572ffb69ab709305c92a3aa7f762
Phillip Ross <phillipross@users.noreply.github.com>
2025-01-12 23:57:45 -0500

    Maven Wrapper (#114)
    - removed maven install/config from GHA workflow and associated shell script
    - removed maven from .sdkmanrc
    - switch GHA and bitbucket docker images from maven to openjdk
    - updated CI commands to invoke maven w/ mvnw instead of mvn
    
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>

--- .github/install-sdkman.sh
+++ .github/install-sdkman.sh
@@ -3,7 +3,6 @@
 set -euf -o pipefail
 
 JAVA_VERSION=$1
-MAVEN_VERSION=$2
 
 printf "Installing, configuring, and initializing SDKMAN\n"
 curl -s "https://get.sdkman.io" | bash
@@ -13,6 +12,3 @@ source "${HOME}/.sdkman/bin/sdkman-init.sh"
 
 printf "Installing Azul Zulu JDKs via SDKMAN\n"
 sdk i java ${JAVA_VERSION}
-
-printf "Installing Apache Maven via SDKMAN\n"
-sdk i maven ${MAVEN_VERSION}
