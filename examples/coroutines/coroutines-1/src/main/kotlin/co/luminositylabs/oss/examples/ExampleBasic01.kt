4bfaece798b5bf08165692ab7d54ab50785c82de
Phillip Ross <phillipross@users.noreply.github.com>
2025-03-03 00:31:12 -0500

    Updates (#131)
    - http4k updated from v6.0.0.0 to v6.0.1.0
    - removed NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE suppression from ExampleBasic01.kt in coroutines-1 module as it is no longer necessary
    
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>

--- examples/coroutines/coroutines-1/src/main/kotlin/co/luminositylabs/oss/examples/ExampleBasic01.kt
+++ examples/coroutines/coroutines-1/src/main/kotlin/co/luminositylabs/oss/examples/ExampleBasic01.kt
@@ -1,6 +1,5 @@
 package co.luminositylabs.oss.examples
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -10,7 +9,6 @@ val logger = KotlinLogging.logger {}
 
 fun main() = ExampleBasicNonBlocking().nonBlockingHelloWorld()
 
-@SuppressFBWarnings("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
 class ExampleBasicNonBlocking {
     fun nonBlockingHelloWorld() {
         runBlocking {
